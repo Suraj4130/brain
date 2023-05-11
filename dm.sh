b3b62aa488bb13b3dcb6956a9501592c662f3b5c
Suraj4130 <133203024+Suraj4130@users.noreply.github.com>
2023-05-11 13:26:22 +0530

    Create dm.sh

---
+++ dm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python -m pip install --upgrade pip
+pip3 install flask
+pip3 install --upgrade Flask
+pip3 install opencv-python
+pip3 install flask_sqlalchemy
+pip3 install PIL
+pip3 install numpy
+pip3 install keras
+pip3 install gunicorn
